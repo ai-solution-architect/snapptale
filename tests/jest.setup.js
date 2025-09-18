3a7e6a22ecbf98b19e9197fee94a6cce3d5da594
Jonatas Emidio de Souza <jonatassouza@Mac.lan>
2025-09-18 23:57:23 +0100

    fix(pdf): fix scrolling

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,2 +1,7 @@
 // tests/jest.setup.js
 require('dotenv').config({ path: './.env.local' });
+
+// Mock scrollIntoView for JSDOM environment, only if window is defined
+if (typeof window !== 'undefined') {
+  window.HTMLElement.prototype.scrollIntoView = jest.fn();
+}
